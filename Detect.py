735f69543efb0542d7057ef3074d103e515aff00
WHK-Scbert <63253621+WHK-Scbert@users.noreply.github.com>
2023-02-09 00:02:34 -0700

    Test Docker on Rasp pi

---
+++ Detect.py
@@ -0,0 +1,13 @@
+from fdlite import FaceDetection, FaceDetectionModel
+from fdlite.render import Colors, detections_to_render_data, render_to_image 
+from PIL import Image
+
+image = Image.open('group2.jpg')
+detect_faces = FaceDetection(model_type=FaceDetectionModel.BACK_CAMERA)
+faces = detect_faces(image)
+if not len(faces):
+    print('no faces detected :(')
+else:
+    render_data = detections_to_render_data(faces, bounds_color=Colors.GREEN)
+    render_to_image(render_data, image).show()
+    print('Face Detected')
